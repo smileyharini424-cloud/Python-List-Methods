6b3119eae8851e56f01905e649bebcb4be86e376
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 23:44:35 -0700

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,21 @@
+numbers = [30, 10, 50, 20, 40]
+
+print("Original List:", numbers)
+
+numbers.append(60)
+print("After Append:", numbers)
+
+numbers.insert(2, 25)
+print("After Insert:", numbers)
+
+numbers.remove(40)
+print("After Remove:", numbers)
+
+numbers.pop()
+print("After Pop:", numbers)
+
+numbers.sort()
+print("After Sort:", numbers)
+
+numbers.reverse()
+print("After Reverse:", numbers)
